5d98f57dd8f39560896b5feae4590ce13a2cb58d
Will Britt <will.britt@centre.edu>
2019-03-26 14:47:57 -0400

    a

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+dataP = d3.json("classData.json");
 
+dataP.then(function(data)
+{
+  console.log("data",data)
+
+
+  var xScale = d3.scaleLinear()
+              .domain(d3.extent(data))
+              .nice()
+
+  var binMaker = d3.histogram()
+                .domain.(xScale.domain())
+                .threshold(xScale.ticks(50));
+
+var bins = binMaker(data);
+
+setup(data,xScale,binMaker,bins)
+
+},
+
+function(err)
+{console.log(err);}
+
+)
+
+
+var setup = function(data,xScale,binMaker,bins)
+{
+  var svg = d3.select("svg")
+    .attr("width", 600)
+    .attr("height",400)
+}
