728904bdd04eeadbba9b6cea44b1b08e862491f5
Courtney Gibson <courtney.gibson@centre.edu>
2019-03-28 15:42:17 -0400

    edit

--- index.js
+++ index.js
@@ -47,15 +47,25 @@ dataP.then(function(data)
 var bins = binMaker(newData);
 
 var barWidth = width/bins.length;
-
+makeButtons(newData)
 setup(data,xScale,yScale,binMaker,bins,width,height,barWidth,margins)
-
+update(data,xScale,yScale,binMaker,bins,width,height,barWidth,margins)
 },
 
 function(err)
 {console.log(err);}
 
 )
+
+var makeButtons = function(data)
+{
+  d3.select("body").selectAll("button")
+    .data(data)
+    .enter()
+    .append("button")
+    .text(function(d){return "Day: "+d.day})
+    //.attr("on", update(d))
+}
 
 var getData = function(data, dayIndex)
 {
@@ -71,6 +81,45 @@ var setup = function(data,xScale,yScale,binMaker,bins,w,h,barWidth,margins)
   var svg = d3.select("svg")
     .attr("width", 600)
     .attr("height",400)
+
+    var xAxis  = d3.axisBottom(xScale)
+                .scale(xScale)
+                .tickValues([5,15,25,35,45])
+                .tickFormat(function(d,i){return ""+bins[i].x0+"-"+bins[i].x1+"";})
+
+
+
+    var yAxis  = d3.axisLeft(yScale);
+
+    svg.append("g")
+           .classed(xAxis,true)
+           .call(xAxis)
+           .attr("transform","translate("+margins.left+","
+           +(margins.top+h)+")"
+        );
+
+
+     svg.append("g")
+       .classed(yAxis,true)
+       .call(yAxis)
+       .attr("transform","translate("+(margins.left-10)+","
+       + 5 +")");
+
+console.log(bins)
+
+    svg.selectAll("rect")
+        .data(bins)
+        .enter()
+        .append("rect")
+        .attr("height", function(d,i){return h-yScale(d.length);})
+        .attr("width", barWidth)
+        .attr("x", function(d, i){return i*(w*5/(data.length))+margins.left-18;})
+        .attr("y", function(d, i){return h-yScale(data.length-d.length)+margins.top-5;})
+}
+
+var update = function(data,xScale,yScale,binMaker,bins,w,h,barWidth,margins)
+{
+  var svg = d3.select("svg")
 
     var xAxis  = d3.axisBottom(xScale)
                 .scale(xScale)
